300e27dc135522f87fa500df44dba76b6a053e07
JesusHerreraAl <jherreraalvarado@educa.madrid.org>
2026-09-17 16:10:36 +0200

    Purchase avanzada

--- src/tiendaJuegos/Purchase.java
+++ src/tiendaJuegos/Purchase.java
@@ -3,5 +3,41 @@ package tiendaJuegos;
 public class Purchase {
 	
 	//REPRESENTA LAS COMPRAS DE LA TIENDAS QUE SE GUARDAN 
+	private Customer customer; //CLIENTE 
+	private Game game;
+	private int quantity; //CANTIDAD
+	private double totalPrice; //TOTAL DE LO QUE SE GASTO EN LOS JUEGOS
+	
+	public Purchase (Customer customer, Game game, int quantity) {
+		this.customer=customer;
+		this.game=game;
+		this.quantity=quantity;
+		this.totalPrice=game.getPrice()*quantity;
+	}
+	
+	public Customer getCustomer() {
+		return customer;
+	}
+	public Game getGame(){
+		return game;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Purchase [customer=" + customer + ", game=" + game + ", quantity=" + quantity + ", totalPrice="
+				+ totalPrice + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
